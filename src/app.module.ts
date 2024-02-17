133538474eee11fbf68fd8b50d4a0def594bfe02
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2024-02-17 19:45:08 +0100

    feat: 🎸 Adding ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // TODO : validate environment with zod
+    // TODO : abstrat config module to not depends on it
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
