fa40e70e3d1bb7163db884554b86fee9eda4649b
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2024-02-17 19:55:42 +0100

    feat: 🎸 Adding RepositoryPort

---
+++ src/libs/ddd/repository.port.ts
@@ -0,0 +1,32 @@
+import { Option } from 'oxide.ts';
+
+export class Paginated<T> {
+  readonly count: number;
+  readonly limit: number;
+  readonly page: number;
+  readonly data: readonly T[];
+
+  constructor(props: Paginated<T>) {
+    this.count = props.count;
+    this.limit = props.limit;
+    this.page = props.page;
+    this.data = props.data;
+  }
+}
+
+export type OrderBy = { field: string | true; param: 'ASC' | 'DESC' };
+
+export type PaginatedQueryParams = {
+  limit: number;
+  page: number;
+  offset: number;
+  orderBy: OrderBy;
+};
+
+export interface RepositoryPort<Entity> {
+  insert(entity: Entity | Entity[]): Promise<void>;
+  findOneById(id: string): Promise<Option<Entity>>;
+  findAll(): Promise<Entity[]>;
+  findAllPaginated(params: PaginatedQueryParams): Promise<Paginated<Entity>>;
+  delete(entity: Entity): Promise<boolean>;
+}
