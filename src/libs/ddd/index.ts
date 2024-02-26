7fd34c8c7e1e9fc6420a79ed64dbb48dbba6550b
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2024-02-26 20:59:14 +0100

    refactor: 💡 Updating libs/ddd barrel file

--- src/libs/ddd/index.ts
+++ src/libs/ddd/index.ts
@@ -1,4 +1,6 @@
+export * from './aggregate-root.base';
 export * from './command.base';
+export * from './domain-event.base';
 export * from './entity.base';
 export * from './mapper.interface';
 export * from './query.base';
