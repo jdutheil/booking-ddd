83551d16c5c3aa9d783b938aab53ac8c0fe2d133
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2024-03-03 16:20:48 +0100

    refactor: 💡 Use repository instead of PrismaService

--- src/infrastructure/rest-api/authentication/application/queries/get-booker-id-for-authentication/get-booker-id-for-authentication.handler.ts
+++ src/infrastructure/rest-api/authentication/application/queries/get-booker-id-for-authentication/get-booker-id-for-authentication.handler.ts
@@ -1,28 +1,32 @@
+import { Inject } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { PrismaService } from '@src/infrastructure/prisma/prisma.service';
 import { EntityID } from '@src/libs/ddd';
 import { Err, Ok, Result } from 'oxide.ts';
+import {
+  AUTHENTICATION_REPOSITORY,
+  AuthenticationRepositoryPort,
+} from '../../ports/authentication.repository.port';
 import { GetBookerIdForAuthenticationQuery } from './get-booker-id-for-authentication.query';
 
 @QueryHandler(GetBookerIdForAuthenticationQuery)
 export class GetBookerIdForAuthenticationQueryHandler
   implements IQueryHandler<GetBookerIdForAuthenticationQuery>
 {
-  // TODO : Move to a repository !!
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    @Inject(AUTHENTICATION_REPOSITORY)
+    private authenticationRepository: AuthenticationRepositoryPort,
+  ) {}
 
   async execute(
     query: GetBookerIdForAuthenticationQuery,
   ): Promise<Result<EntityID, Error>> {
-    const authentication = await this.prismaService.authentication.findUnique({
-      where: { id: query.authenticationId },
-      select: { bookerId: true },
-    });
+    const authenticationResult =
+      await this.authenticationRepository.findOneById(query.authenticationId);
 
-    if (!authentication) {
+    if (authenticationResult.isNone()) {
       return Err(new Error('Authentication not found'));
     }
 
-    return Ok(authentication.bookerId);
+    return Ok(authenticationResult.unwrap().bookerId);
   }
 }
