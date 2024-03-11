df769342a1583a5ab530af666d01eab834c0713d
Jérémy Dutheil <dutheil.jeremy@gmail.com>
2024-03-11 07:20:59 +0100

    feat: 🎸 Link everything in Organizer Module

--- src/modules/organizer/organizer.module.ts
+++ src/modules/organizer/organizer.module.ts
@@ -1,11 +1,19 @@
 import { Module, Provider } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PrismaModule } from '@src/infrastructure/prisma/prisma.module';
+import { CreateOrganizerService } from './application/commands/create-organizer/create-organizer.service';
 import { OrganizerMapper } from './domain/organizer.mapper';
+import { OrganizerPrismaRepository } from './infrastructure/persistence/organizer.prisma.repository';
+import { ORGANIZER_REPOSITORY } from './infrastructure/persistence/organizer.repository';
 
-const commandHandlers: Provider[] = [];
+const commandHandlers: Provider[] = [CreateOrganizerService];
 const mappers: Provider[] = [OrganizerMapper];
-const repositories: Provider[] = [];
+const repositories: Provider[] = [
+  {
+    provide: ORGANIZER_REPOSITORY,
+    useClass: OrganizerPrismaRepository,
+  },
+];
 
 @Module({
   imports: [PrismaModule, CqrsModule],
